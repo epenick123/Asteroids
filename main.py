0d28c9757e0809902c6f9491362fca6fc8d4f4e4
epenick123 <baseball_man25852@hotmail.com>
2024-09-03 15:40:42 -0700

    Added player triangle to GUI spawning in center of window, added A and D key rotation functionality to rotate player direction

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main () :
     running = True
@@ -11,14 +12,18 @@ def main () :
     clock = pygame.time.Clock()
     dt = 0  
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 
         screen.fill((0,0,0))
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()    
         dt = (clock.tick(60)) / 1000
+       
     pygame.quit()
 if __name__ == "__main__":
         main()
